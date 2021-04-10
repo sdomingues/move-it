8f76b132bd44e5814e022eb0cef0e6ea05c7bad7
Samuel Domingues <samuelfdominguess@gmail.com>
2021-04-10 15:07:34 -0300

    adding profile.tsx

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/sdomingues.png" alt="Samuel Domingues"></img>
+            <div>
+                <strong>Samuel Domingues</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
