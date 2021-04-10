5daa32d1ab414234c743da3cd3060d99ea4c5495
Samuel Domingues <samuelfdominguess@gmail.com>
2021-04-10 14:40:32 -0300

    first React / Next project commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+
+      <ExperienceBar />
+
+      <section>
+        
+      </section>
+    </div>
+  );
+}
