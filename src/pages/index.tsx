7b54b0efe3170a3c6899975d397975bf1fa21ac2
Samuel Domingues <samuelfdominguess@gmail.com>
2021-04-11 13:56:14 -0300

    sunday's commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,22 @@ import Head from 'next/head';
 import { Profile } from "../components/Profile";
 import styles from "../styles/pages/Home.module.css"
 import { CompletedChallanges } from "../components/CompletedChallanges";
+import { Countdown } from "../components/Countdown";
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
+      <Head>
+        <title>App | Move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
             <Profile />
             <CompletedChallanges />
+            <Countdown />
         </div>
         <div>
 
